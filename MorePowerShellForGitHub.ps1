67176175a522d286a5dedf0e586a242d92ba41e8
Sofian Aamri <sofianaamri@hotmail.com>
2021-06-08 15:28:16 +0200

    modified:   MorePowerShellForGitHub.ps1  Scripts afgewerkt (functies etc.)

--- MorePowerShellForGitHub.ps1
+++ MorePowerShellForGitHub.ps1
@@ -1,25 +1,254 @@
-﻿Clear-Host
+﻿<#
+	.SYNOPSIS
+		Set-GitHubPersonalAccessToken
+	.DESCRIPTION
+		Saves the supplied personal access token for use in GitHub API interactions
+		
+	.LINK
+		https://github.com/settings/tokens
+	.PARAMETER  token
+		The Personal Access token value supplied by GitHub to be used to identify the user account when perfoming API requests
+	.EXAMPLE
+		PS C:\> Set-GitHubPersonalAccessToken -token "abjkhi121121kjkjk"
+#>
+function Set-GitHubPersonalAccessToken(){
 
-$username = 'testUser'
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]
+		[String] $token
+	)
 
-$password = ConvertTo-SecureString "Sofian-1235" -AsPlainText -Force
-
-
-$C = New-Object system.management.automation.pscredential ($username, $password)
-
-Invoke-RestMethod -method POST -uri ("https://api.github.com/user") -Credential $C
-
-function Get-AuthHeader 
-{
-
-param([System.Management.Automation.PSCredential]$Credential)
-    
-    if ($password -eq "ditisezpz123") {
-        $AuthHash = @{ Username = $Credential.UserName; pswd = $Credential.Password; usr_agnt= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
-        return $AuthHash
-    }
-    Write-Output "Wrong password"
-    return $null
+	[Environment]::SetEnvironmentVariable("GitHubPersonalAccessToken",$token,"User")
 }
 
-Get-AuthHeader -Credential $C
+<#
+	.SYNOPSIS
+		Set-GitHubPersonalAccountName
+	.DESCRIPTION
+		Saves the supplied personal GitHub profile name for use in GitHub API interactions
+		
+	.LINK
+		https://github.com/settings/profile
+	.PARAMETER  userName
+		The profile name to be used to identify the user account when perfoming API requests
+	.EXAMPLE
+		PS C:\> Set-GitHubPersonalAccessToken -token "abjkhi121121kjkjk"
+#>
+function Set-GitHubPersonalAccountName(){
+
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]
+		[String] $userName
+	)
+
+	[Environment]::SetEnvironmentVariable("GitHubPersonalAccountName",$userName,"User")
+}
+
+function Get-AuthHeader(){
+	$token= GitHubPersonalAccessToken
+	$headers = @{}                                                                                                                                                                                     
+	$headers.Add("Authorization","Bearer $token")
+	return $headers
+}
+
+<#
+	.SYNOPSIS
+		New-GitHubRepo
+	.DESCRIPTION
+		Creates a new GitHub repo on a user account
+		
+	.LINK
+		https://developer.github.com/v3/repos/#create
+	.PARAMETER  repoName
+		The name of the repo to be created
+	.PARAMETER  private
+		Creates the repo as private when set
+	
+	.EXAMPLE
+		PS C:\> New-GitHubRepo -repoName "MyRepo"
+		Creates a new public repo on GitHub named "MyRepo", for the specified user
+	.EXAMPLE
+		PS C:\> New-GitHubRepo -repoName "MyRepo" -private
+		Creates a new private repo on GitHub named "MyRepo", for the specified user
+#>
+function New-GitHubRepo(){
+
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]
+		[String]$repoName,
+		[Parameter()]
+		[switch]$private = $false
+	)
+
+	if(-NOT(Token-Present))
+	{
+		return;
+	}
+	
+	$body= @{ 
+		name="$repoName"; 
+		description="test description"; 
+		homepage="https://github.com"; 
+		private= $private; 
+		gitignore_template = "VisualStudio" 
+		}
+		
+	$j = (ConvertTo-Json $body)
+	$headers = Get-AuthHeader
+
+	Invoke-WebRequest  -Uri "https://api.github.com/user/repos" -Method Post -Body $j -Headers $headers -UseBasicParsing
+}
+
+<#
+	.SYNOPSIS
+		Add-Collaborator
+	.DESCRIPTION
+		Adds GitHubUsers as collaborrators to an existing GitHub repository
+		
+	.LINK
+		https://developer.github.com/v3/repos/collaborators/#add-user-as-a-collaborator
+	.PARAMETER  repoName
+		The name of the repo to be deleted
+	.PARAMETER  gitHubUserNames
+		A list of gitHub usernames to be added as collaborator
+	
+	.EXAMPLE
+		PS C:\> Add-Collaborator -repoName "MyRepo" -gitHubUserName "johnsmith"
+		Adds GitHub username JohnSmith as the collaborator to the repo MyRepo
+	
+#>
+function Add-GitHubCollaborator(){
+
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]
+		[String] $repoName,
+		[Parameter(Mandatory=$true)]
+		[String[]] $gitHubUserNames
+	)
+		
+	if(-NOT(Token-Present))
+	{
+		return;
+	}
+
+	$headers = Get-AuthHeader
+	$headers.Add("Content-Length",0)	
+	
+	$accountName = Get-GitHubPersonalAccountName
+
+	foreach ($username in $gitHubUserNames)
+	{
+	
+		$url = "https://api.github.com/repos/$accountName/$repoName/collaborators/$username"
+	
+		write-host "Adding $username as collaborator to repo $repoName"	
+	
+		Invoke-WebRequest  -Uri $url -Method PUT -Headers $headers -UseBasicParsing                                                                     
+	}
+}
+
+<#
+	.SYNOPSIS
+		Remove-GitHubRepo
+	.DESCRIPTION
+		deletes a GitHub repo
+		
+	.LINK
+		https://developer.github.com/v3/repos/#delete-a-repository
+	.PARAMETER  repoName
+		The name of the repo to be deleted
+	
+	.EXAMPLE
+		PS C:\> Remove-GitHubRepo -repoName "MyRepo"
+		Creates a new public repo on GitHub named "MyRepo", for the specified user
+	
+#>
+function Remove-GitHubRepo(){
+
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true)]
+		[String]$repoName
+	)
+  
+	if(-NOT(Token-Present))
+	{
+		return;
+	}
+  
+	$headers = Get-AuthHeader
+	
+	$accountName = Get-GitHubPersonalAccountName
+	
+	Invoke-WebRequest  -Uri "https://api.github.com/repos/$accountName/$repoName" -Method DELETE -Headers $headers -UseBasicParsing
+   
+}
+
+<#
+	.SYNOPSIS
+		Clear-GitHubPersonalAccessToken
+	.DESCRIPTION
+		rtemoves the saved personal access token for use in GitHub API interactions
+		
+	.LINK
+		https://github.com/settings/tokens
+	.PARAMETER  token
+		The Personal Access token value supplied by GitHub to be used to identify the user account when perfoming API requests
+	.EXAMPLE
+		PS C:\> Clear-GitHubPersonalAccessToken
+#>
+    function Clear-GitHubPersonalAccessToken(){
+	[Environment]::SetEnvironmentVariable("GitHubPersonalAccessToken",$null,"User")
+}
+
+<#
+	.SYNOPSIS
+		Clear-GitHubPersonalAccountName
+	.DESCRIPTION
+		removes the profile name used in GitHub API interactions
+		
+	.LINK
+		https://github.com/settings/profile
+	
+	.EXAMPLE
+		PS C:\> Clear-GitHubPersonalAccountName
+#>
+function Clear-GitHubPersonalAccountName(){
+	[Environment]::SetEnvironmentVariable("GitHubPersonalAccountName",$null,"User")
+}
+
+function Get-GitHubPersonalAccessToken(){
+	[Environment]::GetEnvironmentVariable("GitHubPersonalAccessToken","User")
+}
+
+function Get-GitHubPersonalAccountName(){
+	[Environment]::GetEnvironmentVariable("GitHubPersonalAccountName","User")
+}
+
+function Token-Present(){
+
+	$ret = $true
+
+	if((Get-GitHubPersonalAccessToken) -eq $null){
+		Write-Host "Personal Access Token NOT set"  -ForegroundColor DarkYellow
+		Write-Host "Create a personal access token at https://github.com/settings/tokens then use Set-GitHubPersonalAccessToken to save it as an environment variable for future use."
+		
+		$ret = $false
+	}
+	
+	
+	if((Get-GitHubPersonalAccountName) -eq $null){
+		Write-Host "GitHub username NOT set"  -ForegroundColor DarkYellow
+		Write-Host "Use Set-GitHubPersonalAccountName to save it as an environment variable for future use."
+		
+		$ret = $false
+	}
+	
+	
+	return $ret
+	
+}
